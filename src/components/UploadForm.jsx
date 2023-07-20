cbe4ac8eeab04305bbf719859e002eddab0c54ce
MohsenSami <mohsensami88@gmail.com>
2023-07-20 11:48:37 +0330

    basic file input

---
+++ src/components/UploadForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+    const types = ['image/jpeg', 'image/png'];
+    const handleChange = (e) => {
+        let selected = e.target.files[0];
+
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError('Please select an image file (png or jpeg)');
+        }
+    };
+    return (
+        <form>
+            <input type="file" onChange={handleChange} />
+            <div className="output">
+                {error && <div className="error">{error}</div>}
+                {file && <div>{file.name}</div>}
+            </div>
+        </form>
+    );
+};
+
+export default UploadForm;

e5e87c7105ca5334c771d4a21b8a3d986b3ad7fe
mohsensami <mohsensami18727@gmail.com>
2024-05-22 23:09:29 +0330

    v1: simples shuffle array

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 // const myArray = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
 // const shuffledArray = shuffle(myArray);
 // console.log(shuffledArray);
-function shuffle(array) {
+
+// function isWds(string) {
+//     return string === 'WDS';
+// }
+
+// module.exports = isWds;
+
+function makeShuffle(array) {
     return array.sort(() => Math.random() - 0.5);
 }
 
-module.exports = shuffle;
+module.exports = makeShuffle;

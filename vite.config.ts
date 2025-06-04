628ceaacfc1f789ab69d48cf5f7b37bca3d542ac
mohsensami <mohsensami18727@gmail.com>
2025-06-04 20:46:43 +0330

    fix bug in vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "index.ts"),
+      entry: resolve(__dirname, "index.ts"),
       name: "react-loading-skeleton",
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format) => `react-loading-skeleton.${format}.js`,
+      formats: ["es", "umd"],
     },
     rollupOptions: {
       external: ["react", "react-dom"],
@@ -18,10 +23,22 @@ export default defineConfig({
           react: "React",
           "react-dom": "ReactDOM",
         },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "style.css")
+            return "react-loading-skeleton.css";
+          return assetInfo.name || "unknown";
+        },
       },
     },
     sourcemap: true,
     emptyOutDir: true,
   },
-  plugins: [react(), dts(), libInjectCss()],
+  plugins: [
+    react(),
+    dts({
+      include: ["src"],
+      exclude: ["**/*.test.ts", "**/*.test.tsx"],
+    }),
+    libInjectCss(),
+  ],
 });
